c497b90ba9e40f0a0a5d96d38521f4039764def1
unknown <hafizulhanifr@gmail.com>
2026-06-23 18:10:59 +0700

    Change ExportService to standard HTTP Response to fix Cloud 500 error

--- app/Services/ExportService.php
+++ app/Services/ExportService.php
@@ -2,20 +2,20 @@
 
 namespace App\Services;
 
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Http\Response;
 
 class ExportService
 {
     /**
-     * Export video data to a CSV StreamedResponse.
+     * Export video data to a CSV Response.
      * Opens perfectly in Excel, Google Sheets, etc.
      *
      * @param string $username
      * @param string $platform
      * @param array|\Illuminate\Support\Collection $videos
-     * @return StreamedResponse
+     * @return Response
      */
-    public function exportToCsv(string $username, string $platform, $videos): StreamedResponse
+    public function exportToCsv(string $username, string $platform, $videos): Response
     {
         $fileName = "analisis_{$platform}_{$username}_" . date('Ymd_His') . ".csv";
 
@@ -29,49 +29,49 @@ class ExportService
 
         $columns = ['No', 'Video ID', 'Caption/Deskripsi', 'Tanggal Posting', 'Jumlah Views', 'Jumlah Likes', 'Jumlah Komentar', 'Engagement Rate (%)', 'URL Video'];
 
-        $callback = function() use($videos, $columns) {
-            $file = fopen('php://output', 'w');
-            
-            // Prepend UTF-8 BOM to ensure Excel displays Indonesian language, special characters, and emojis properly
-            fprintf($file, chr(0xEF).chr(0xBB).chr(0xBF));
-            
-            fputcsv($file, $columns);
+        $output = fopen('php://temp', 'r+');
+        
+        // Prepend UTF-8 BOM to ensure Excel displays Indonesian language, special characters, and emojis properly
+        fwrite($output, chr(0xEF).chr(0xBB).chr(0xBF));
+        
+        fputcsv($output, $columns);
 
-            foreach ($videos as $index => $video) {
-                // If it's an object (Eloquent Model)
-                if (is_object($video)) {
-                    $row = [
-                        $index + 1,
-                        $video->video_id,
-                        $video->caption,
-                        $video->post_date->format('Y-m-d H:i:s'),
-                        $video->views,
-                        $video->likes,
-                        $video->comments,
-                        $video->engagement_rate,
-                        $video->video_url
-                    ];
-                } else {
-                    // If it's an array
-                    $row = [
-                        $index + 1,
-                        $video['video_id'] ?? '',
-                        $video['caption'] ?? '',
-                        $video['post_date'] ?? '',
-                        $video['views'] ?? 0,
-                        $video['likes'] ?? 0,
-                        $video['comments'] ?? 0,
-                        $video['engagement_rate'] ?? 0.0,
-                        $video['video_url'] ?? ''
-                    ];
-                }
-                fputcsv($file, $row);
+        foreach ($videos as $index => $video) {
+            // If it's an object (Eloquent Model)
+            if (is_object($video)) {
+                $row = [
+                    $index + 1,
+                    $video->video_id,
+                    $video->caption,
+                    $video->post_date->format('Y-m-d H:i:s'),
+                    $video->views,
+                    $video->likes,
+                    $video->comments,
+                    $video->engagement_rate,
+                    $video->video_url
+                ];
+            } else {
+                // If it's an array
+                $row = [
+                    $index + 1,
+                    $video['video_id'] ?? '',
+                    $video['caption'] ?? '',
+                    $video['post_date'] ?? '',
+                    $video['views'] ?? 0,
+                    $video['likes'] ?? 0,
+                    $video['comments'] ?? 0,
+                    $video['engagement_rate'] ?? 0.0,
+                    $video['video_url'] ?? ''
+                ];
             }
+            fputcsv($output, $row);
+        }
 
-            fclose($file);
-        };
+        rewind($output);
+        $csvContent = stream_get_contents($output);
+        fclose($output);
 
-        return response()->stream($callback, 200, $headers);
+        return response($csvContent, 200, $headers);
     }
 
     /**
@@ -80,9 +80,9 @@ class ExportService
      * @param string $username
      * @param string $platform
      * @param array $plannerData
-     * @return StreamedResponse
+     * @return Response
      */
-    public function exportPlannerToCsv(string $username, string $platform, array $plannerData): StreamedResponse
+    public function exportPlannerToCsv(string $username, string $platform, array $plannerData): Response
     {
         $fileName = "content_planner_3bulan_{$platform}_{$username}_" . date('Ymd_His') . ".csv";
 
@@ -96,29 +96,29 @@ class ExportService
 
         $columns = ['Bulan', 'Minggu', 'Kategori/Tema', 'Ide Video / Judul', 'Hook (3 Detik Pertama)', 'Konsep Visual & Transisi', 'Draf Caption & Hashtags'];
 
-        $callback = function() use($plannerData, $columns) {
-            $file = fopen('php://output', 'w');
-            
-            // UTF-8 BOM
-            fprintf($file, chr(0xEF).chr(0xBB).chr(0xBF));
-            
-            fputcsv($file, $columns);
+        $output = fopen('php://temp', 'r+');
+        
+        // UTF-8 BOM
+        fwrite($output, chr(0xEF).chr(0xBB).chr(0xBF));
+        
+        fputcsv($output, $columns);
 
-            foreach ($plannerData as $item) {
-                fputcsv($file, [
-                    $item['month'] ?? '',
-                    $item['week'] ?? '',
-                    $item['theme'] ?? '',
-                    $item['title'] ?? '',
-                    $item['hook'] ?? '',
-                    $item['visual'] ?? '',
-                    $item['caption'] ?? ''
-                ]);
-            }
+        foreach ($plannerData as $item) {
+            fputcsv($output, [
+                $item['month'] ?? '',
+                $item['week'] ?? '',
+                $item['theme'] ?? '',
+                $item['title'] ?? '',
+                $item['hook'] ?? '',
+                $item['visual'] ?? '',
+                $item['caption'] ?? ''
+            ]);
+        }
 
-            fclose($file);
-        };
+        rewind($output);
+        $csvContent = stream_get_contents($output);
+        fclose($output);
 
-        return response()->stream($callback, 200, $headers);
+        return response($csvContent, 200, $headers);
     }
 }
